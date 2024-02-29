ac67b3e045fdd2fb5419b10bc4ec5152fc4c5ba9
Your Name <shivamarora.07.sa@gmail.com>
2024-02-29 15:17:41 +0530

    update item1.jsx

--- src/components/item1.jsx
+++ src/components/item1.jsx
@@ -6,10 +6,10 @@ function Item1() {
   return (
     <div className='grid grid-cols-2'>
       <div className='h-48 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
+        <div className='h-1/2'>
         <img className='object-cover' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
         </div>
-        <div className='h-2/5 my-1'>
+        <div className='h-1/2 my-1'>
             <div className='bg-white'>
                 <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
                 <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
