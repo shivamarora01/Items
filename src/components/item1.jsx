726ba2d658c6effdc57c8392db6b6cc980ebc809
Your Name <shivamarora.07.sa@gmail.com>
2024-02-29 15:13:24 +0530

    update item1.jsx

--- src/components/item1.jsx
+++ src/components/item1.jsx
@@ -5,16 +5,15 @@ import { CiHeart } from "react-icons/ci";
 function Item1() {
   return (
     <div className='grid grid-cols-2'>
-      <div className='h-48 border-solid border-black'>
+      <div className='h-48 border border-solid border-neutral-600'>
         <div className='h-3/5'>
         <img className='object-cover' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
         </div>
         <div className='h-2/5 my-1'>
             <div className='bg-white'>
                 <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                
                 <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 mx-1 px-1 rounded-sm'>Add to Cart</button>
+                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1'>Add to Cart</button>
             </div>
         </div>
       </div>
