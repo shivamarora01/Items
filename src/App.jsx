a1edfebe4bd8f93b217b6dfd4b8ef092a6d2a4b6
Your Name <shivamarora.07.sa@gmail.com>
2024-02-29 14:50:41 +0530

    items created in two by two matrix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Item1 from './components/item1'
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <Item1/>
     </>
   )
 }
---
+++ src/components/item1.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { LiaRupeeSignSolid } from "react-icons/lia";
+import { CiHeart } from "react-icons/ci";
+
+function Item1() {
+  return (
+    <div className='bg-red-900 grid grid-cols-2'>
+      <div className='bg-blue-800 h-48'>
+        <div className='bg-yellow-500 h-3/5'>
+        <img className='object-cover' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
+        </div>
+        <div className='bg-yellow-800 h-2/5'>
+            <div className='bg-white'>
+                <h1 className=' bg-red-500 flex flex-start mx-1'>Sandel<span className='bg-blue-800 ml-14 my-1 text-xl'><CiHeart /></span></h1>
+                
+                <h1 className='bg-purple-700 flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
+                <button className='bg-yellow-700 mx-1 px-1 rounded-sm'>Add to Cart</button>
+            </div>
+        </div>
+      </div>
+      <div className='bg-green-800 h-48'>
+      <img src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="" />
+      </div>
+      <div className='bg-yellow-800 h-48'>
+      <img src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="" />
+      </div>
+      <div className='bg-purple-800 h-48'>
+      <img src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="" />
+      </div>
+      <div className='bg-blue-800 h-48'>Hi</div>
+      <div className='bg-green-800 h-48'>Hi</div>
+      <div className='bg-yellow-800 h-48'>Hi</div>
+      <div className='bg-purple-800 h-48'>Hi</div>
+      <div className='bg-green-800 h-48'>Hi</div>
+      <div className='bg-yellow-800 h-48'>Hi</div>
+      <div className='bg-purple-800 h-48'>Hi</div>
+    </div>
+  )
+}
+
+export default Item1
