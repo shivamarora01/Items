da891da81ef9afee35267b25af54b7910d25c0a4
Your Name <shivamarora.07.sa@gmail.com>
2024-03-01 10:32:53 +0530

    update item1.jsx

--- src/components/item1.jsx
+++ src/components/item1.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import {React,useState} from 'react'
 import { LiaRupeeSignSolid } from "react-icons/lia";
+import { FaHeart } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 
 function Item1() {
+  
+  const [heartIcon,setHeartIcon] = useState(false)
+  const handleIconClick = () => {
+    console.log("Heart Clicked!!")
+      setHeartIcon(!heartIcon)
+  }
   return (
     <div className='grid grid-cols-2'>
-
-
+       <div className='h-56 border border-solid border-neutral-600'>
+        <div className='h-3/5'>
+        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
+        </div>
+        <div className='h-2/5 my-2'>
+            <div className='bg-white'>
+              <div className='flex justify-between mx-1'>
+                <h1 className=''>Sandel</h1>
+                    <h1 className='my-1 text-xl' onClick={handleIconClick}>
+                      {heartIcon ? <FaHeart className='text-red-500'/> : <CiHeart/>}
+                    </h1>
+              </div>
+              <div className='flex flex-start mx-1'>
+                <h1 className='mt-1'><LiaRupeeSignSolid /></h1>
+                <h1 className='text-md'>799      <span className='text-sm line-through'>(1299)</span>        <span className='text-red-700 font-semibold text-sm'>38%</span></h1>
+              </div>
+                <button className='bg-yellow-400 px-1 rounded-sm flex flex-start mx-1 mt-1 text-sm'>Add to Cart</button>
+            </div>
+        </div>
+      </div>
       <div className='h-56 border border-solid border-neutral-600'>
         <div className='h-3/5'>
         <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
@@ -18,7 +43,7 @@ function Item1() {
                 <h1 className='my-1 text-xl'><CiHeart /></h1>
               </div>
               <div className='flex flex-start bg-blue-400 mx-1'>
-                <h1 className='mt-1'><LiaRupeeSignSolid /></h1>
+                <h1 className='mt-1' onClick={handleIconClick}>{heartIcon}</h1>
                 <h1 className='text-sm'>799</h1>
               </div>
                 <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 mt-1 text-sm'>Add to Cart</button>
@@ -33,145 +58,13 @@ function Item1() {
             <div className='bg-white'>
               <div className='flex justify-between mx-1'>
                 <h1 className=''>Sandel</h1>
-                <h1 className='my-1 text-xl'><CiHeart /></h1>
+                <h1 className='my-1 text-xl'>{heartIcon}</h1>
               </div>
               <div className='flex flex-start mx-1'>
                 <h1 className='mt-1'><LiaRupeeSignSolid /></h1>
                 <h1 className='text-md'>799      <span className='text-sm line-through'>(1299)</span>        <span className='text-red-700 font-semibold text-sm'>38%</span></h1>
               </div>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 mt-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className='bg-red-500 flex flex-start mx-1'>Sandel<span className='bg-blue-500 ml-14 my-1 text-xl '><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>    
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
-            </div>
-        </div>
-      </div>     
-      <div className='h-56 border border-solid border-neutral-600'>
-        <div className='h-3/5'>
-        <img className='object-cover h-full w-full' src="https://mulltiply-prod.s3.ap-south-1.amazonaws.com/uploads/2024/01/48e9ddd2-b761-11ee-b10d-694ead45503e.webp" alt="No Image Found" />
-        </div>
-        <div className='h-2/5 my-2'>
-            <div className='bg-white'>
-                <h1 className=' flex flex-start mx-1'>Sandel<span className='ml-14 my-1 text-xl'><CiHeart /></span></h1>
-                <h1 className='flex flex-start mx-1'><LiaRupeeSignSolid /><span>799</span></h1>
-                <button className='bg-yellow-500 px-1 rounded-sm flex flex-start mx-1 text-sm'>Add to Cart</button>
+                <button className='bg-yellow-400 px-1 rounded-sm flex flex-start mx-1 mt-1 text-sm'>Add to Cart</button>
             </div>
         </div>
       </div>
